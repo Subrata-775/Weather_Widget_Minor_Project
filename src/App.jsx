3c6f579f436877697516054c3924afc9bb641225
Subrata Jana <subratajana7735@gmail.com>
2026-05-03 21:25:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import PrimarySearchAppBar from '../src/Components/NavBar'
+import MediaCard from '../src/Components/MediaCard'
+
+function App() {
+
+  return (
+    <>
+      <PrimarySearchAppBar />
+      <MediaCard />
+
+    </>
+
+
+  )
+}
+
+export default App;
